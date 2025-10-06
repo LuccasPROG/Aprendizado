b8867c8ee281830c01b6c3f9ee96ed5c5e75310e
LuccasPROG <lucaseviliton2019@gmail.com>
2025-10-06 15:58:51 -0300

    aula 52 editada e aula 53 função recursiva

--- aulas/aula52.py
+++ aulas/aula52.py
@@ -1,7 +1,30 @@
-def recursiva (inicio=0, fim=4):
+# Funções recursivas e recursividade
+# - funções que podem se chamar de volta
+# - úteis p/ dividir problemas grandes em partes menores
+# Toda função recursiva deve ter:
+# - Um problema que possa ser dividido em partes menores
+# - Um caso recursivo que resolve o pequeno problema
+# - Um caso base que para a recursão
+#____________________________________________________________#
+# def recursiva (inicio=0, fim=4):
+# # função recursiva contar numeros ate o final
+#     print(inicio, fim)
+#     if inicio >= fim:
+#         return fim
+    
+#     inicio += 1
+#     return recursiva(inicio, fim)
 
+# #Inicio: 
 
-    return recursiva(inicio, fim)
+# print(recursiva(0,2000))
+#____________________________________________________________#
+#Funções Recursivas!
+def factorial(n):
+    if n <= 1:
+        return 1
+    
+    return n * factorial(n -1)
 
-
-print(recursiva)
+# Inicio:
+print(factorial(5))
