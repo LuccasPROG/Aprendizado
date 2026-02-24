de9f128307a5e5f61702ae2dfdae5fc7886beb88
LuccasPROG <lucaseviliton2019@gmail.com>
2026-02-24 11:49:50 -0300

    adiconando projeto de estoque

--- projetos_pessoais/teste.py
+++ projetos_pessoais/teste.py
@@ -1,1 +1,25 @@
-print("OI")
+def criar_funcao(func):
+    
+    def interna(*args, **kwargs):
+        for arg in args:
+            e_string(args)
+
+        resultado = func(*args, **kwargs)
+
+        return resultado
+    
+    return interna
+
+
+def inverte_string(string):
+    return string[::-1]
+
+
+def e_string(parametro):
+    if not isinstance(parametro, str):
+        raise TypeError('ERROR: Digite uma letra Não Número!')
+
+inverte_string_checando_parametro = criar_funcao(inverte_string)
+
+invertida = inverte_string_checando_parametro('123')
+print(invertida)
