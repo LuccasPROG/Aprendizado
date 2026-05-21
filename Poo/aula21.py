9ce9532a159f05ea02f56a945148d461d38bc069
LuccasPROG <lucaseviliton2019@gmail.com>
2026-05-21 17:50:10 -0300

    adicionando aulas

--- Poo/aula21.py
+++ Poo/aula21.py
@@ -1,11 +1,24 @@
 class MyError(Exception):
     ...
+class Outroerror(Exception):
+    ...
 
 
 
 def levantar():
-    raise MyError('A mensagem do meu erro')
+    exception_ = MyError('A mensagem do meu erro')
+    exception_.add_note('VOcê errou isso !!')
+    raise exception_
 
-levantar()
+try:
+    levantar()
+except (MyError, ZeroDivisionError) as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    print()
+    exception_ = Outroerror('vou lançar de novo')
+    exception_.__notes__ = error.__notes__.copy()
+    exception_.add_note('mais um error!')
+    raise exception_ from error
 
 
---
+++ Poo/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+    # print('p2 é maior que p1', p2 > p1)
