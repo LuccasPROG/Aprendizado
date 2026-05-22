27637d28699af244d98d49ff31f9944a12d20c90
LuccasPROG <lucaseviliton2019@gmail.com>
2026-05-22 15:31:03 -0300

    Adicionando aula 26 e 25

---
+++ Poo/aula26.py
@@ -0,0 +1,21 @@
+from contextlib import contextmanager
+
+
+
+@contextmanager
+def my_open(caminho_arquivo, modo):
+    try:
+        print('Abrindo arquivo')
+        arquivo = open(caminho_arquivo, modo, encoding='utf8')
+        yield arquivo
+    except Exception as e:
+        print('Ocorreu erro', e)    
+    finally:
+        print("fechando arquivo")
+
+
+with my_open('aula26.txt', 'w',) as arquivo:
+    arquivo.write('linha 1  \n')
+    arquivo.write('linha 2  \n', 123)
+    arquivo.write('linha 3  \n')
+    print('WITH', arquivo)
