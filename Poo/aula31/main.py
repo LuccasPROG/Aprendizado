8bba484d4ca76b8c4d3a4e3ae8a89bfe9750f3c6
LuccasPROG <lucaseviliton2019@gmail.com>
2026-06-03 14:10:04 -0300

    adicionando projetos

--- Poo/aula31/main.py
+++ Poo/aula31/main.py
@@ -1,7 +1,9 @@
-import uma_linha
+# import uma_linha
+# import varias_linhas
+import documentando_funcoes
 
 # print(dir(uma_linha)) # dir mostra tudo que tem dentro do import/modulo !
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
 
-help(uma_linha)
+help(documentando_funcoes)
