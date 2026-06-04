9cde8a7b0fe9f47e36b462d24e1ed1dd0641c4a9
LuccasPROG <lucaseviliton2019@gmail.com>
2026-06-04 17:56:30 -0300

    Adicionando Atividade Banco

---
+++ Poo/aulabanco/contas.py
@@ -0,0 +1,56 @@
+import abc
+
+class Conta(abc.ABC):
+
+    def __init__(self, agencia: int, conta: int, saldo: float =0) -> None:
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = saldo
+
+    @abc.abstractmethod
+    def sacar(self, valor:float) -> float:
+        ...
+
+    def depositar(self, valor: float) -> float:
+        self.saldo += valor
+        self.detalhes(f'(DEPÒSITO {valor})')
+        return self.saldo
+    
+
+    def detalhes(self, msg: str='') -> None:
+        print(f'O seu saldo é {self.saldo:.2f} {msg}')    
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f'(SAQUE NEGADO{valor})')
+        return self.saldo
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia: int, conta: int, saldo:float=0, limite:float=0) -> float:
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo = -self.limite
+
+        if valor_pos_saque >= limite_maximo:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
+        print('Não foi possível sacar o valor desejado')
+        print(f'Seu Limite é {-limite_maximo:.2f}')
+        self.detalhes(f'(SAQUE NEGADO {valor})')
+        return self.saldo
+
+if __name__ == '__main__':
+    cc1 = ContaCorrente(111, 222, 0, 100)
+    cc1.sacar(1)
