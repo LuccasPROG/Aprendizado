4bdb9144b161d90d8f28c923bfe90d20fe2787c5
LuccasPROG <lucaseviliton2019@gmail.com>
2026-07-03 16:39:50 -0300

    Adicionando aula 11

---
+++ Modulos/aula11.py
@@ -0,0 +1,48 @@
+import math
+import os
+from itertools import count
+
+
+def formata_tamanho(tamanho_em_bytes: int, base: int = 1000) -> str:
+    """Formata um tamanho, de bytes para o tamanho apropriado"""
+
+    # Original:
+    # https://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
+
+    # Se o tamanho for menor ou igual a 0, 0B.
+    if tamanho_em_bytes <= 0:
+        return "0B"
+
+    # Tupla com os tamanhos
+    #                      0    1     2     3     4     5
+    abreviacao_tamanhos = "B", "KB", "MB", "GB", "TB", "PB"
+    # Logaritmo -> https://brasilescola.uol.com.br/matematica/logaritmo.htm
+    # math.log vai retornar o logaritmo do tamanho_em_bytes
+    # com a base (1000 por padrão), isso deve bater
+    # com o nosso índice na abreviação dos tamanhos
+    indice_abreviacao_tamanhos = int(math.log(tamanho_em_bytes, base))
+    # Por quanto nosso tamanho deve ser dividido para
+    # gerar o tamanho correto.
+    potencia = base ** indice_abreviacao_tamanhos
+    # Nosso tamanho final
+    tamanho_final = tamanho_em_bytes / potencia
+    # A abreviação que queremos
+    abreviacao_tamanho = abreviacao_tamanhos[indice_abreviacao_tamanhos]
+    return f'{tamanho_final:.2f} {abreviacao_tamanho}'
+
+from itertools import count
+
+caminho = os.path.join('/Users', 'lucas', 'Documents', 'teste')
+counter = count()
+
+for root, dirs, files in os.walk(caminho):
+    the_conter = next(counter)
+    print(the_conter, 'pasta atual', root)
+
+    for dir_ in dirs:
+        print('    ', the_conter, 'Dir:', dir_)
+
+    for file_ in files:
+        caminho_completo_arquivo = os.path.join(root, file_)
+        tamanho = os.path.getsize(caminho_completo_arquivo)
+        print('    ', the_conter, 'files:', file_, formata_tamanho(tamanho))
