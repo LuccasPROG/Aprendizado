7254462750f43723c02ab1ecae331015584d5ccb
LuccasPROG <lucaseviliton2019@gmail.com>
2026-07-29 17:28:32 -0300

    Organizando Projetos Pessoais

--- projetos_pessoais/numero_multiplicador_com_close_function.py
+++
@@ -1,8 +0,0 @@
-def criar_multiplicador(multiplicador):
-    def interna(numero):
-        print(f'{multiplicador * numero}')
-    return interna
-
-dobro = criar_multiplicador(int(input('Digite o numero multiplicador: ')))
-
-dobro(int(input('Digite o numero que deseja multiplicar: ')))
